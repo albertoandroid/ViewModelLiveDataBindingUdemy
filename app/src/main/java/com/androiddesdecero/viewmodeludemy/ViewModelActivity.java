c0c3f4797bc53a145662e6b6b2cbbae6dfe8914d
Alberto <info@androiddesdecero.com>
2019-06-22 18:23:02 +0200

    03

--- app/src/main/java/com/androiddesdecero/viewmodeludemy/ViewModelActivity.java
+++ app/src/main/java/com/androiddesdecero/viewmodeludemy/ViewModelActivity.java
@@ -3,12 +3,38 @@ package com.androiddesdecero.viewmodeludemy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.androiddesdecero.viewmodeludemy.util.Sumar;
 
 public class ViewModelActivity extends AppCompatActivity {
+
+    private TextView tvSumar;
+    private TextView tvSumarViewModel;
+    private Button btSumar;
+    private int resultado;
+    //private SumarViewModel sumarViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_model);
+
+        setUpView();
+    }
+
+    private void setUpView(){
+        tvSumar = findViewById(R.id.tvSumar);
+        tvSumarViewModel = findViewById(R.id.tvSumarViewModel);
+        btSumar = findViewById(R.id.btSumar);
+        btSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultado = Sumar.sumar(resultado);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/androiddesdecero/viewmodeludemy/util/Sumar.java
@@ -0,0 +1,8 @@
+package com.androiddesdecero.viewmodeludemy.util;
+
+public class Sumar {
+    public static final int sumar(int numero){
+        numero = numero + 1;
+        return numero;
+    }
+}
